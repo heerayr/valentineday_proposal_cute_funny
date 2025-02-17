70c62f61160155b70f4f68432d577c1eaf397c45
Heera YR <131390921+heerayr@users.noreply.github.com>
2025-02-17 15:43:52 +0530

    Delete components/ui directory

--- components/ui/card.js
+++
@@ -1,8 +0,0 @@
-export function Card({ children }) {
-    return <div className="bg-white p-4 rounded shadow">{children}</div>;
-  }
-  
-  export function CardContent({ children }) {
-    return <div className="p-2">{children}</div>;
-  }
-  
